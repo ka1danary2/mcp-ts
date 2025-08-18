a9b7785fee2e8b0c89a6a7689b957b2981f4a387
Potega Xenia <goldenksenia145@gmail.com>
2025-08-18 22:12:16 +0800

    cli работа в консоли

--- src/entrypoint/cli.ts
+++ src/entrypoint/cli.ts
@@ -3,10 +3,42 @@
 import { ChatProcessor } from "../ai/processor";
 import { EntryPointInterface } from "./interface";
 
-export class CliEntrypoint implements EntryPointInterface {
-    constructor(private readonly processor: ChatProcessor) { }
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
 
-    run(): Promise<void> | void {
-        console.log('cli start');
+export class CliEntrypoint implements EntryPointInterface {
+    constructor(private readonly processor: ChatProcessor) {
+
+    }
+
+    async run() {
+        const SESSION_ID = 'cli-session';
+        console.log("CLI mode started");
+        // Здесь будет логика CLI  
+
+        const rl = readline.createInterface({ input, output });
+
+        while (true) {
+            const query = await rl.question('\n🗣️  Ваш запрос: ');
+            if (query.trim().toLowerCase() === 'exit') {
+                console.log('👋 До свидания!');
+                rl.close();
+                process.exit(0);
+            }
+            const start = Date.now();
+            console.log('🤖 Думаю...');
+
+            const response = await this.processor.processMessage(SESSION_ID, query);
+            const end = Date.now();
+            const durationSec = ((end - start) / 1000).toFixed(2);
+
+            console.log(`\n🤖 AI (${durationSec} сек):\n${response.message}`);
+            if (response.tools.length > 0) {
+                console.log(`🛠️  Использованные инструменты:`);
+                response.tools.forEach((tool, i) => {
+                    console.log(`  ${i + 1}. ${tool.name} ${JSON.stringify(tool.arguments)}`);
+                });
+            }
+        }
     }
 }
